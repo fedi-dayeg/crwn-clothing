47fbf783c25497248e296f880e2058acbb83e294
fedi <fedi.dayeg@hotmail.com>
2019-11-24 14:17:09 +0100

    add redux to the project

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.style.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../../custom-button/custom-button.component";
-import {signInWithGoogle} from "../../firebase/firebase.utils";
+import {auth, signInWithGoogle} from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component{
     constructor(props) {
@@ -13,10 +13,17 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password:''})
-        console.log(event);
+        //console.log(event);
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password:''});
+        }catch (e) {
+            console.log('Error', e)
+
+        }
     };
 
     handleChange = event => {
