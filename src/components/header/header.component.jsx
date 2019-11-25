151077c749dc39319eb2d2f8d0b1b79a2cdb9c55
fedi <fedi.dayeg@hotmail.com>
2019-11-26 00:03:39 +0100

    added cartIcon and cart-dropdown component to our application, also implimented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,11 @@ import {auth} from "../../firebase/firebase.utils";
 import {connect} from 'react-redux';
 import {setCurrentUser} from "../../redux/user/user.actions";
 
-const Header = ({currentUser}) => (
+import CartIcon from "../cart-icon/cart-icon.component";
+
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo'/>
@@ -16,17 +20,21 @@ const Header = ({currentUser}) => (
             <Link className='option' to='/shop'> SHOP </Link>
             <Link className='option' to='/shop'> CONTACT </Link>
             {
-                currentUser ?
+                currentUser ? (
                     <div className='option' onClick={() => auth.signOut()}> SIGN OUT</div>
-                    :
+                ) : (
                     <Link className='option' to='/signin'> SIGN IN</Link>
-            }
+                )}
+            <CartIcon/>
         </div>
+        {hidden ? null : <CartDropdown/>}
+
     </div>
 );
 
-const mapStateTpProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateTpProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 
