47554245c3dc508ea7e44a2146196c01938681e1
fedi <fedi.dayeg@hotmail.com>
2019-12-21 13:36:58 +0100

    moved our directory and shop data inti its own reducer created respective selectors and update corresponding componint with new redux flow for directory/shop

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import './category.style.scss';
+
+
+const CategoryPage = ({match}) => (
+    <div className='category'>
+        <h2>CATEGORY PAGE</h2>
+    </div>
+);
+
+export default CategoryPage;
+
+
